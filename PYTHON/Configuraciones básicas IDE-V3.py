94c763c1117afe3b76f120bf54b6c0d66627a941
kevinQL <unajmakev@gmail.com>
2017-12-24 15:30:33 -0500

    Sintaxis Básica I

---
+++ PYTHON/Configuraciones básicas IDE-V3.py
@@ -0,0 +1,8 @@
+#aquí iría un comentario
+print ("HOLA MUNDIO")
+
+mi_nombre = "kevin quispe lima"
+
+## CONFIGRURA SUBLIME TEXT 3
+## tools/command palette/"Install Package Control"/ Package Control: "Install Package"/ "SublimeREPL".
+## tools/SublimeREPL/Python/Python
