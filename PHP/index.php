64394f92de1822b9612245d4a4ef248adcd29d35
KevinQL <unajmakev@gmail.com>
2018-01-10 13:27:20 -0500

    configuraciones básicas
    git-bass
    variables de entorno 
    editor de texto

---
+++ PHP/index.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+*	CONFIGURACIONES BÁSICAS
+
+###
+manejador de sistemas
+
+###
+el usarios es : root
+#la contraseña es la que se configura
+
+
+## para ejecutar php7 desde la terminal::
+En el panel de control:
+> sistema y seguridad > sistema > configuración avanzada del sistema >
+opciones avanzadas > variables de entorno
+buscar Path || enter 
+pegar ruta después del punto y coma
+
+
+## Alias para un php7
+	Antes tenesmo que copiarla ruta del php7.exe en el consoloa de comandos
+	> /Applications/mampstack-7.0.3-0/php/bin/php -v
+	### version php 7 ...
+	> which php7  ## Asigna la ruta completa a la variable php7
+	### php7: aliased to /Applica...
+
+
+
+----------------- USUO GIT-BASH ----------------
+
+### versión php
+> php -v 
+
+### limpiar consola
+>Ctrl+l
+
+### servir php
+> php -S localhost:8080
+/// 
+	la S tiene que ser mayuscula.
+	puede ser cualquier puerto que este en escucha, sin uso
+
+*/
+
+echo "hola mundo";
+
+
+?>
